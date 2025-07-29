1a58d3490a8073b1f78229707f6eb85cf49df792
Suraj Kumar <sk658139@gmail.com>
2025-07-29 13:13:07 +0530

    added logo and launcher activity

---
+++ app/src/main/java/com/surajvanshsv/chatapp/Launcher.java
@@ -0,0 +1,37 @@
+package com.surajvanshsv.chatapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.surajvanshsv.chatapp.views.LoginActivity;
+
+public class Launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_launcher);
+
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(Launcher.this, LoginActivity.class));
+            finish();  // optional: remove Launcher from backstack
+        }, 2000);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
